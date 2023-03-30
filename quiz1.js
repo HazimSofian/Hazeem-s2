043ecaab377c4c8bc3dc35b6591c3091515e6cb5
Your Name <you@example.com>
2023-03-30 15:52:06 +0800

    readme

---
+++ quiz1.js
@@ -0,0 +1,36 @@
+let message = "benr"
+//console.log(message, typeof message)
+
+message = 123
+//console.log(message, typeof message)
+
+
+let str = "1.258"
+let num = 1
+
+let total = parseFloat(str) + num 
+//console.log(total, typeof total)
+
+let str1 = "1"
+let num1 = 1
+
+let total1 = parseInt(str1) + num1 
+//console.log(total1, typeof total1)
+
+
+let data = ["benr", 3532, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name: "Hazim",
+    age: 23,
+    faculty: "FKEKK"
+}
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
